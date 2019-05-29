e9beddd7b084dce4ea35695723bc51543ff0de8f
Rafal Brzoska <rbrzoska@future-processing.com>
2019-05-29 09:28:37 +0200

    App navigation + skeleton

--- fp-proj/src/app/app-routing.module.ts
+++ fp-proj/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'contact', component: ContactComponent },
+  { path: 'articles/:id', component: ArticleComponent },
+  { path: 'articles', component: ArticleListComponent },
+  { path: 'admin', component: AdminComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fp-proj/src/app/app.module.ts
+++ fp-proj/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { AdminComponent } from './admin/admin.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ArticleComponent,
+    ArticleListComponent,
+    AdminComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
