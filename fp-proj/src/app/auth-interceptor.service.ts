f811329fc5bb2ae99f71e460dd22956dd9c6d299
Rafal Brzoska <rbrzoska@future-processing.com>
2019-05-30 08:34:24 +0200

    Interceptor observable

--- fp-proj/src/app/auth-interceptor.service.ts
+++ fp-proj/src/app/auth-interceptor.service.ts
@@ -2,27 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { AuthService } from './auth.service';
-import { switchMap } from 'rxjs/operators';
+import { exhaustMap, finalize, mergeMap, switchMap, take, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
-  token = null;
   constructor(private authService: AuthService) {
-    this.authService.isLoggedIn$.subscribe(
-      isLoggedIn => {
-        this.token = isLoggedIn ? this.authService.token : null
-      }
-    )
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let changedReq = this.token ? req.clone({
-      setHeaders: {
-        Authorization: "Bearer " + this.token
-      }
-    }) : req;
-
-    return next.handle(changedReq);
+    return this.authService.isLoggedIn$.pipe(
+      take(1),
+      switchMap(isLogged => isLogged ? next.handle(req.clone({
+        setHeaders: {
+          Authorization: "Bearer " + this.authService.token
+        }
+      })) : next.handle(req) ),
+      tap(console.log)
+    );
   }
 }
--- fp-proj/src/app/auth.guard.ts
+++ fp-proj/src/app/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate  {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.isLoggedIn$
+    return this.authService.isLoggedIn$.pipe(tap(() =>console.log('guard')))
   }
 
 }
